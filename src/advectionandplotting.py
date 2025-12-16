7497d8bdb4cf5b59b4c5c15eb6ca31402ceb830b
rachhb <rachel.borcherds@student.manchester.ac.uk>
2025-12-16 17:06:39 +0000

    Add assertion for Nt
    Added assertion to check that Nt is not zero

--- src/advectionandplotting.py
+++ src/advectionandplotting.py
@@ -9,9 +9,12 @@ dx = spatialResolution
 CFLmax = 1
 dt = CFLmax * dx / u
 
-plotInterval = math.ceil(int(temporalRes / dt)) # rounds up to prevent plot interval being 0
+plotInterval = int(math.ceil(temporalRes / dt)) # rounds up to prevent plot interval being 0
 
 Nt = int(totalT / dt)
+
+# check Nt isn't 0
+assert Nt != 0
 
 Nx = interpLength
 
@@ -29,10 +32,16 @@ for j in range( 1, Nt + 1 ):
     # boundary conditions
     y[0] = yOld[0] # continuous inflow of pollutant
     y[-1] = y[-2] # downstream boundary: 0 spatial gradient to allow outflow
+    raise ValueError('The current end boundary condition causes the graph to go flat at the end.')
     
     if j % plotInterval == 0:
         plt.plot(x, y)
 
 plt.xlabel("Distance")
 plt.ylabel("Concentration")
+plt.show()
+
+plt.plot(x, newC, label="Initial")
+plt.plot(x, y, label="Final")
+plt.legend()
 plt.show()
