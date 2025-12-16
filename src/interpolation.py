0934ba587463548b032e8a369c023d2c46eb962b
rachhb <rachel.borcherds@student.manchester.ac.uk>
2025-12-16 16:23:19 +0000

    Refactor river length and interpolation handling
    Updated river length calculation and interpolation logic.

--- src/interpolation.py
+++ src/interpolation.py
@@ -1,19 +1,21 @@
-riverLength = Distance[20] # e.g. 20 m
-spatialResolution = 0.2 # 20 cm
+# variable for the what number row the last row is, adjusting for first row being Distance=0
+lastRow = len(Distance)-1
+
+# gets river length from file
+riverLength = (Distance[lastRow]) # e.g. len(Distance)=21, Distance[20]=20m
+
+numberOfInterpolations = float(input("Input the number of interpolations required here. e.g. input 5 for a spatial resolution of 0.2: "))
 
 # assume that the initial resolution is the same for the whole file
 # everything in metres
 initialResolution = Distance[1] - Distance[0]
-numberOfInterpolations = initialResolution / spatialResolution
 
 # e.g. inital length 21 becomes 101 when adding 4 interpolation points
 interpLength = int((len(Concentration) - 1) * numberOfInterpolations + 1)
-print(interpLength)
-print(numberOfInterpolations)
 
 # interpolating first column
 # again assuming evenly spaced
-newD = np.linspace(Distance[0], Distance[20], num=interpLength)
+newD = np.linspace(Distance[0], Distance[lastRow], num=interpLength)
 
 # interpolating second column using newD
 newC = np.interp(newD, Distance, Concentration)
